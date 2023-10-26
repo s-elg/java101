7a95c4d63429de2ab8b8ae275fa3c553bfd1335e
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2023-10-26 12:48:45 +0300

    Add files via upload

---
+++ LoopsAverage.java
@@ -0,0 +1,29 @@
+package assignments;
+
+import java.util.Scanner;
+public class LoopsAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         int number, average, total = 0, totalNumbers = 0;
+
+        System.out.print("enter a number: ");
+        number = input.nextInt();
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                totalNumbers++;
+            } else {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("-------------");
+
+        average = total/totalNumbers;
+        System.out.println("your average: " + average);
+
+
+    }
+}
